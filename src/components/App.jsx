5601cd9d105655fa10d6469646bdd7a9b6d49d67
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-04-20 00:01:32 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,13 +5,13 @@ import { ContactList } from "./ContactList/ContactList";
 export class App extends Component {
   state = {
     contacts: [
-      // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
     ],
     filter: "",
-  }
+  };
 
   addContact = newContact => {
     this.setState(prevState => ({
@@ -19,16 +19,22 @@ export class App extends Component {
     }));
   };
 
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
   changeFilter = event => {
     this.setState({ filter: event.target.value });
-  }
+  };
 
   getSearchContact = () => {
     const { filter, contacts } = this.state;
     const normalizedSearch = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedSearch));
-  }
+  };
 
   render() {
     const { filter } = this.state;
