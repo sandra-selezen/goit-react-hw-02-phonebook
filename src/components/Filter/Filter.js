239e07cc0e08c1967163173b81c0b8503a706445
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-04-23 20:02:31 +0300

    Update Filter

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,7 +1,8 @@
 // import { Formik, Field } from 'formik';
 // import * as yup from 'yup';
 // import { nanoid } from 'nanoid';
-
+import { SearchLabel } from './Filter.styled';
+import { RiUserSearchFill } from "react-icons/ri";
 
 // let schema = yup.string();
 // const searchId = nanoid();
@@ -11,7 +12,7 @@ export const Filter = ({ value, onChange }) => {
   return (
     // <Formik initialValues={initialValues} value={value} onChange={onChange} validationSchema={schema}>
     <>
-      <label htmlFor="1">Find contact by name </label>
+      <SearchLabel htmlFor="1"><RiUserSearchFill /> Find contact by name </SearchLabel>
       <input value={value} onChange={onChange} id="1"/>
     </>
 
---
+++ src/components/Filter/Filter.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const SearchLabel = styled.label`
+  margin-bottom: 10px;
+  svg {
+    margin-right: 8px;
+  }
+`;
+
+// export const SearchInput = styled.input`
+  
+
+// `;
